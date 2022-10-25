5a97d756cac326dcff7974ed684be37a095ee3eb
xA6V5x <ojedaagustinmanuel@gmail.com>
2022-10-25 01:41:53 -0300

    next Ây nest

---
+++ src/pages/home.tsx
@@ -0,0 +1,29 @@
+import { GetServerSideProps } from 'next';
+import Link from 'next/link';
+import { FC } from 'react';
+import { BlogPost } from 'src/shared/types/blog-post';
+
+type THomeProps = {
+  blogPosts: BlogPost[];
+};
+
+const HomeExample: FC<THomeProps> = ({ blogPosts = [] }) => {
+  return (
+    <div>
+      <h1>Home Example</h1>
+      {blogPosts.map(({ title, id }) => (
+        <div key={id}>
+          <Link href={`/${id}`}>{title}</Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<THomeProps> = async (
+  ctx,
+) => {
+  return { props: {} };
+};
+
+export default HomeExample;
