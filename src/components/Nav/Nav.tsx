0651fc53183977614ee053028cf0f79926d8dda5
xA6V5x <ojedaagustinmanuel@gmail.com>
2022-10-31 16:27:02 -0300

    Nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,9 +1,22 @@
+import Link from 'next/link';
+import { NavContainer, NavOptions } from 'src/styles/NavStyles';
+
 const Nav = () => {
   return (
-    <div>
-      <h1>Nav Example</h1>
-    </div>
+    <NavContainer>
+      <NavOptions>
+        <h1>Your Store</h1>
+        <input type="text" placeholder="search products..." />
+        <Link href="/Login">Login</Link>
+        <Link href="/SingUp">Sing Up</Link>
+      </NavOptions>
+      <NavOptions>
+        <Link href="/categories">Categories</Link>
+        <Link href="/offers">Offers</Link>
+        <Link href="/popular">Popular</Link>
+      </NavOptions>
+    </NavContainer>
   );
 };
 
-export default Nav
+export default Nav;
---
+++ src/styles/NavStyles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const NavContainer = styled.div`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: auto;
+  background-color: #637dd8;
+`;
+
+export const NavOptions = styled.div`
+  display: flex;
+  margin: 10px 0;
+  justify-content: space-around;
+  margin: auto;
+  font-size: 12px;
+`;
