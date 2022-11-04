2bcfba754c6ca359f59c6c771a4f7db42aef1c7e
xA6V5x <ojedaagustinmanuel@gmail.com>
2022-11-03 21:34:42 -0300

    listo

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -1,13 +1,13 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 const HeadGlobal = () => {
-    return (
-        <Head>
-            <title>Your Store</title>
-            <meta name="description" content="Your Store" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-    );
+  return (
+    <Head>
+      <title>Your Store</title>
+      <meta name="description" content="Your Store" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
 };
 
 export default HeadGlobal;
