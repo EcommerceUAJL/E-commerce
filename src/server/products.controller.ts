c9017b83126bbf97ad16cac4a6ddf5af058b4fb4
luciano plaza <lucianomps2015@gmail.com>
2022-11-10 20:39:36 -0300

    feat: update product & getById modal

--- src/server/products.controller.ts
+++ src/server/products.controller.ts
@@ -1,18 +1,29 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { ProductsService } from './products.service';
-import { CreateProductDTO } from './DTOs/createProduct.dto';
+import { CreateProductDTO } from './DTOs/Product.dto';
+import { EditProductDTO } from './DTOs/editProduct.dto';
 @Controller()
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Get()
   // @Render('index')
-  home() {
+  homeProductsBestRankeds() {
     return this.productsService.getProductsHome();
   }
 
+  // @Get(':id')
+  // getProductById(@Param('id') Id: string) {
+  //   return this.productsService.getProductById(Id);
+  // }
+
+  @Put(':id')
+  editProductById(@Param('id') Id: string, @Body() body: EditProductDTO) {
+    return this.productsService.updateProduct(Id, body);
+  }
+
   @Post('/create')
-  create(@Body() body: CreateProductDTO) {
+  createProduct(@Body() body: CreateProductDTO) {
     return this.productsService.createProduct(body);
   }
 }
--- src/server/products.service.ts
+++ src/server/products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { CreateProductDTO } from './DTOs/createProduct.dto';
+import { CreateProductDTO } from './DTOs/Product.dto';
+import { EditProductDTO } from './DTOs/editProduct.dto';
 @Injectable()
 export class ProductsService {
   async getProductsHome() {
@@ -29,4 +30,36 @@ export class ProductsService {
       data: { ...products },
     };
   }
+
+  async updateProduct(id: string, body: EditProductDTO) {
+    const productsDB = await fetch('https://dummyjson.com/products')
+      .then((response) => response.json())
+      .then((data) => data.products);
+    let productUpdated = productsDB.find((p: any) => p.id === parseInt(id, 10));
+
+    productUpdated = {
+      title: body.title ? body.title : productUpdated.title,
+      description: body.description
+        ? body.description
+        : productUpdated.description,
+      price: body.price ? body.price : productUpdated.price,
+      stock: body.stock ? body.stock : productUpdated.stock,
+      brand: body.brand ? body.brand : productUpdated.brand,
+      category: body.category ? body.category : productUpdated.category,
+      thumbnail: body.thumbnail ? body.thumbnail : productUpdated.thumbnail,
+      images: body.images ? body.images : productUpdated.images,
+    };
+
+    return {
+      message: 'Succes Product updated',
+      newProduct: { ...productUpdated },
+    };
+  }
+
+  // async getProductById(id: string) {
+  //   const product = await fetch('https://dummyjson.com/products')
+  //     .then((response) => response.json())
+  //     .then((data) => data.products.find((product: any) => product.id === id));
+  //   return product;
+  // }
 }
